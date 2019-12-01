f0f639f954a237c4d7cc919743e3b50128826d8e
Baren <baren1998@gmail.com>
2019-12-02 02:20:04 +0700

    Add true print result

--- src/js/components/FinishComponent.js
+++ src/js/components/FinishComponent.js
@@ -7,11 +7,43 @@ export default class FinishComponent extends React.Component {
         super(props);
     }
 
+    create_result()
+    {
+        let CurrentNode = this.props.computing_tree;
+        let ArrayStates = [];
+        while (CurrentNode !== null) {
+            ArrayStates.unshift(CurrentNode.get_state());
+            CurrentNode = CurrentNode.get_parent();
+        }
+        return ArrayStates;
+    }
+
     render() {
         return (
-            <div>
-                <div>Метод:   {this.props.method}</div>
-                <div>Шагов:   {this.props.computing_steps}</div>
+            <div className={"FinishMainContainer"}>
+                <h3>Метод:   {this.props.method}</h3>
+                <h3>Шагов:   {this.props.computing_steps}</h3>
+                <div className={"RowStatesResults"}>
+                    {this.create_result().map(state => (
+                        <div className={"StateResultBoard"}>
+                            {state.map(row => (
+                                <div className={"StateResultOneRow"}>
+                                    {row.map(value => {
+                                        let PrintValue = value;
+                                        if (PrintValue === 0){
+                                            PrintValue = "";
+                                        }
+                                        return (
+                                            <div className={"StateResultOneBlock"}>
+                                                {PrintValue}
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                            ))}
+                        </div>
+                    ))}
+                </div>
             </div>
         )
     }
